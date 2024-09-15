4a9a5b7ff66cfdd1430f4464c4858c186e1a48c9
entrolEC <euichan1108@gmail.com>
2024-09-15 15:57:56 +0900

    feat: add loading

---
+++ src/app/time/loading.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Loading() {
+    return (
+        <div className='flex h-full w-full justify-center items-center flex-col gap-4'>
+            <div>loading</div>
+        </div>
+    );
+}
