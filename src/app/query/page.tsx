175c55869ec00976c0d3a9d46179d767ff496a04
entrolEC <euichan1108@gmail.com>
2024-09-17 15:15:37 +0900

    feat: query prefetch

--- src/app/query/page.tsx
+++ src/app/query/page.tsx
@@ -1,8 +1,15 @@
 'use client'
 import Link from "next/link";
+import {useQuery} from "@tanstack/react-query";
+import {getWeather} from "@/lib/fetcher";
 
 
 export default function Home() {
+    const timeZone = 'Asia/Seoul'
+    useQuery({
+        queryKey: ['weather', timeZone],
+        queryFn: () => getWeather('Asia/Seoul')
+    })
     return (
         <div className='flex h-full w-full justify-center items-center flex-col gap-4'>
             <p className='font-bold text-4xl'>This is Query Home Page</p>
